b15fc1aa63bc796a363e70e24bbfe0324844fe21
JaredBondoc <64798794+JaredBondoc@users.noreply.github.com>
2022-05-17 05:53:46 +1000

    Update createTables.sql

--- 03-G25/web/db/createTables.sql
+++ 03-G25/web/db/createTables.sql
@@ -125,12 +125,11 @@ CREATE TABLE Orders (
 /* ---------------------------------------------------------------------- */
 
 CREATE TABLE Product (
-    ProductID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
+    ProductID INTEGER NOT NULL,
     ProductName VARCHAR(40) NOT NULL,
     Stock INTEGER NOT NULL,
     Price NUMERIC(10,2) NOT NULL,
     Description VARCHAR(40),
-    Archived INTEGER NOT NULL,
     CONSTRAINT PK_Product PRIMARY KEY (ProductID)
 );
 
